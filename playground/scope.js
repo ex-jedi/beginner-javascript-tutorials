915730284756ab3f698001cc0e23e94e4012e541
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-12-23 08:29:18 +0000

    Function scope

--- playground/scope.js
+++ playground/scope.js
@@ -1,6 +1,27 @@
-/* eslint-disable */
-// Global variable. Available everywhere. conbst & let not attached to window object
+// Global variable. Available everywhere. conbst & let not attached to window object. All other global variables are.
 const first = 'mark';
+// eslint-disable-next-line prefer-const
 let age = 100;
 // var is attached to the window
-var  where = 'here';
+// eslint-disable-next-line no-var
+var where = 'here';
+
+// Also available on the window object
+function sayHI() {
+  console.log('Hi');
+}
+
+function go() {
+  // Function scope. Variables made in here only available in here
+  const hair = 'Black';
+  console.log(hair);
+  // Global variables available here via scope lookup
+  // Function look inside itself for variable first. If it's not here it looks outside.
+  const age = 200;
+  console.log(age);
+}
+
+go();
+
+console.log(are);
+console.log(hair);
