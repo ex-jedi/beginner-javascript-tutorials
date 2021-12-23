7c67e76e23255f9c6e70a6c696a8784018ffe79d
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-12-23 08:19:37 +0000

    Global variables

---
+++ playground/scope.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+// Global variable. Available everywhere. conbst & let not attached to window object
+const first = 'mark';
+let age = 100;
+// var is attached to the window
+var  where = 'here';
