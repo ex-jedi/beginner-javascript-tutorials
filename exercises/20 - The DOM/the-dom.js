de23c427a1ad1bb88947247cda46932645aba469
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-05 09:49:31 +0000

    querySelector on any element

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,4 +1,9 @@
 const p = document.querySelector('p');
-const divs = document.querySelectorAll('div');
+const divs = document.querySelectorAll('.items img');
+const items2 = document.querySelector('.items2');
+// Can use querySelector on any element
+const items2img = items2.querySelector('img');
 console.log(p);
 console.log(divs);
+console.log(items2);
+console.log(items2img);
