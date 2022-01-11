7c4a307d3ffd7611de0b9ad2e759b12eb6713cb3
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-11 09:09:13 +0000

    Creating elements

--- exercises/20 - The DOM/creating.js
+++ exercises/20 - The DOM/creating.js
@@ -1,1 +1,14 @@
-console.log('Hi there');
+// Element created but not on the page yet
+const myParagraph = document.createElement('p');
+myParagraph.textContent = `I am a paragraph`;
+myParagraph.classList.add('special');
+console.log(myParagraph);
+
+const myImage = document.createElement('img');
+myImage.src = 'https://source.unsplash.com/random/500';
+myImage.alt = 'I made a pic';
+console.log(myImage);
+
+const myDiv = document.createElement('div');
+myDiv.classList.add('wrapper');
+console.log(myDiv);
