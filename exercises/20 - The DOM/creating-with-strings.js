efc72fb40c15ba214b372efaa4d27d2cf4ace0bd
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-12 08:52:03 +0000

    Templating

--- exercises/20 - The DOM/creating-with-strings.js
+++ exercises/20 - The DOM/creating-with-strings.js
@@ -1,3 +1,12 @@
 const items = document.querySelector('.items1');
-items.innerHTML = `<h1>Hello There</h1>`;
+const src = 'https://picsum.photos/400"';
+const desc = 'A Pic of Something';
+const myHTML = `
+<div class="wrapper">
+  <h1>Hello ${desc}</h1>
+  <img src="${src}" alt="${desc}" />
+</div>
+`;
+
+items.innerHTML = myHTML;
 console.log(items.innerHTML);
