ad33f95fdde8988263bb4e2ee12a4cf1a77179f2
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-13 08:52:31 +0000

    Nodes

--- exercises/20 - The DOM/traversing.js
+++ exercises/20 - The DOM/traversing.js
@@ -1,2 +1,8 @@
 const paragraph = document.querySelector('.paragraph');
-console.log(paragraph.childNodes);
+console.log('children ', paragraph.children);
+console.log('first element child', paragraph.firstElementChild);
+console.log('last element child', paragraph.lastElementChild);
+console.log('previous element sibling', paragraph.previousElementSibling);
+console.log('next element sibling', paragraph.nextElementSibling);
+console.log('parent element', paragraph.parentElement);
+console.log('child nodes', paragraph.childNodes);
