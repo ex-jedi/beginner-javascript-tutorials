437045ad445b0df4a5240922f741af76350647fa
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-01-20 09:01:03 +0000

    Prevent default

--- exercises/29 - Events/forms.js
+++ exercises/29 - Events/forms.js
@@ -1,2 +1,8 @@
 const bbc = document.querySelector('.bbc');
-bbc.addEventListener('click', (e) => console.log(e));
+bbc.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(e);
+  console.log('Clicked');
+  const pageChange = confirm('This website might be malicious!');
+  if (pageChange) window.location = e.currentTarget.href;
+});
