c7469e9422b2ea1a43ff69a7331d76fa17f2ee47
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-04-28 09:13:21 +0100

    Access

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,4 +1,5 @@
 const baseEndpoint = 'https://recipes.beginnerjavascript.com/api';
+// Need to go to URL in a browser to request temporary access
 const proxy = `https://cors-anywhere.herokuapp.com/`;
 const form = document.querySelector('form.search');
 const recipesGrid = document.querySelector('.recipes');
@@ -6,6 +7,7 @@ const recipesGrid = document.querySelector('.recipes');
 async function fetchRecipes(query) {
   const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
   const data = await res.json();
+  console.log(data);
   return data;
 }
 
